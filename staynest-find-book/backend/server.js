606a7c06094e9818d787bdfb3222fc122b183164
Bhushan-3IT <chaudharicharandas3@gmail.com>
2026-09-02 14:28:28 +0530

    Fix: CORS updated for Vercel frontend

--- staynest-find-book/backend/server.js
+++ staynest-find-book/backend/server.js
@@ -26,8 +26,16 @@ connectDB();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000'],
+  origin: [
+    'http://localhost:5173',
+    'http://localhost:3000',
+    'https://staynest-frontend.vercel.app',
+    'https://staynest-ashen.vercel.app',
+    'https://staynest.vercel.app'
+  ],
   credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
